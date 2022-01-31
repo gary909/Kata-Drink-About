6b310e992561ce8d3852887db531e3f60f5fe414
gary <gary.white@lodgit.com>
2022-01-31 15:48:41 +0100

    code working

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function peopleWithAgeDrink(old) {
-    return '';
+    if (old >= 21 ){
+        return "drink whisky";
+    } else if (old <= 13) {
+        return "drink toddy";
+    } else if (old >= 14 && old <= 17) {
+        return "drink coke";
+    } else if (old >= 18 && old <= 20){
+        return "drink beer";
+    }
 };
 
 console.log(peopleWithAgeDrink(22)); // return 'drink whisky'
+console.log(peopleWithAgeDrink(13)); // return 'drink toddy'
+console.log(peopleWithAgeDrink(18)); // return 'drink beer'
+console.log(peopleWithAgeDrink(15)); // return 'drink coke'
